aaed1026f4b585e43bb8128dc05e3f8cc43be1af
Saideep <ssambaraju@richrelevance.com>
2026-04-13 22:14:01 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,7 +12,7 @@ var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Go kv store client")
 
 	// Uncomment the code below to pass the first stage
 
