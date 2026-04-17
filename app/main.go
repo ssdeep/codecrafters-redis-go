a794235ad12f1baa43b7af1cd27a29666f88fcd9
Saideep <ssambaraju@richrelevance.com>
2026-04-16 23:31:09 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/codecrafters-io/redis-starter-go/internal/resp"
 )
@@ -12,12 +14,31 @@ import (
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
-var storage = make(map[string]string)
+
+type Value struct {
+	name   string
+	expiry int64
+}
+
+var storage = make(map[string]Value)
+
+func cleanup() {
+	for {
+		for k, v := range storage {
+			if v.expiry != -1 {
+				if v.expiry > time.Now().UnixMilli() {
+					delete(storage, k)
+				}
+			}
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Go kv store client")
-
+	go cleanup()
 	// Uncomment the code below to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -54,13 +75,21 @@ func handleConnection(a net.Conn) {
 			switch cmds[0] {
 			case "SET":
 				fmt.Println("Setting ", cmds[1], " to ", cmds[2])
-				storage[cmds[1]] = cmds[2]
+				argSize := len(cmds)
+				var exp int64
+				if argSize > 3 {
+					exp, _ = strconv.ParseInt(cmds[3], 10, 64)
+					exp = exp + time.Now().UnixMilli()
+				} else {
+					exp = -1
+				}
+				storage[cmds[1]] = Value{cmds[2], exp}
 				a.Write([]byte("+OK\r\n"))
 			case "GET":
 				if _, ok := storage[cmds[1]]; !ok {
 					a.Write([]byte("$-1\r\n"))
 				} else {
-					a.Write(resp.EncodeBulkString(storage[cmds[1]]))
+					a.Write(resp.EncodeBulkString(storage[cmds[1]].name))
 				}
 			case "ECHO":
 				a.Write(resp.EncodeBulkString(cmds[1]))
