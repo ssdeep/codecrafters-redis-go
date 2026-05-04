8c6a00c20034c370921ac6d3ca0a674ab483d606
Saideep <ssambaraju@richrelevance.com>
2026-05-03 22:01:20 -0700

    codecrafters submit [skip ci]

--- internal/commands/executor.go
+++ internal/commands/executor.go
@@ -278,7 +278,7 @@ func (l LPopExecutor) Execute(cmds []string, con net.Conn, storage *sync.Map, li
 }
 
 func (l BLPopExecutor) Execute(cmds []string, con net.Conn, storage *sync.Map, listStorage *sync.Map) {
-	if len(cmds) == 4 {
+	if len(cmds) == 3 {
 		timeout, err := strconv.Atoi(cmds[2])
 		if err != nil {
 			fmt.Println("Error parsing timeout: %w", err)
